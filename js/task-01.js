f83d7f03598a90991c530b21d88e69f9f70e2035
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-07-30 22:48:11 +0300

    task 1 refactor

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
-const itemsRef = document.querySelectorAll('.item');
-const itemTitelsRef = Array.from(itemsRef).map(item => item.querySelector('h2'));
-const itemLiListsRef = Array.from(itemsRef).map(item => item.querySelectorAll('li'));
+const showListInfo = itemsSelector => {
+	const itemsRef = document.querySelectorAll(itemsSelector);
+	const itemTitelsRef = Array.from(itemsRef).map(item => item.querySelector('h2'));
+	const itemLiListsRef = Array.from(itemsRef).map(item => item.querySelectorAll('li'));
 
-const showInfo = () => {
 	console.log(`Number of categories: ${itemsRef.length}`);
 	for (let i = 0; i < itemLiListsRef.length; i += 1) {
 		console.log(`\nCategory: ${itemTitelsRef[i].textContent}\nElements: ${itemLiListsRef[i].length}`);
 	}
 };
-showInfo();
+showListInfo('.item');
