4cb1d9365ed61461babaa487390b4e3d6257c30d
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-07-30 23:47:49 +0300

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+function makeItem(itemName) {
+	const itemRef = document.createElement('li');
+	itemRef.textContent = itemName;
+	itemRef.classList.add('item');
+	return itemRef;
+}
+
+function makeList(listSelector, elements) {
+	const listItemsRef = elements.map(makeItem);
+	const listRef = document.querySelector(listSelector);
+	listRef.append(...listItemsRef);
+}
+makeList('#ingredients', ingredients);
