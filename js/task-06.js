9efa12807ff08bbfe44878532d2c04bce31733ea
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-08-01 19:35:56 +0300

    task6 completed

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+function checkInputLength(inputselector) {
+	const inputRef = document.querySelector(inputselector);
+	const requiredLength = Number(inputRef.dataset.length);
+	console.log(requiredLength);
+	inputRef.addEventListener('input', onInputRef);
+
+	function onInputRef(event) {
+		const inputLength = event.currentTarget.value.length;
+
+		inputRef.addEventListener('blur', () => {
+			if (!(inputLength === requiredLength)) {
+				inputRef.classList.add('invalid');
+			} else {
+				inputRef.classList.replace('invalid', 'valid');
+			}
+		});
+	}
+}
+checkInputLength('#validation-input');
