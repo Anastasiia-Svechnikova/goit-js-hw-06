42f91b6075d1c20ab862eb9893cdc5225ddffb19
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-08-02 00:57:43 +0300

    task10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+const boxesRef = document.querySelector('#boxes');
+const controlsRef = boxesRef.previousElementSibling;
+const amountRef = controlsRef.firstElementChild;
+const createBtnRef = amountRef.nextElementSibling;
+const destroyBtnRef = controlsRef.lastElementChild;
+
+createBtnRef.addEventListener('click', onCreateBtnRef);
+destroyBtnRef.addEventListener('click', onDestroyBtnRef);
+
+function onDestroyBtnRef() {
+	boxesRef.innerHTML = '';
+	amountRef.value = 0;
+}
+
+function onCreateBtnRef() {
+	createBoxes(Number(amountRef.value));
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createBoxes(amount) {
+	let width = 30;
+	let height = 30;
+	let markup = ``;
+
+	for (let i = 0; i < amount; i += 1) {
+		markup += `<div style="background-color: ${getRandomHexColor()}; width:${width}px; height: ${height}px; margin: 5px"></div>`;
+		height += 10;
+		width += 10;
+	}
+
+	boxesRef.insertAdjacentHTML('afterbegin', markup);
 }
