6d78dbc6dcdd3894925f81b18345293c3759150f
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-08-01 23:51:54 +0300

    task9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const changeColorBtnRef = document.querySelector('.change-color');
+const colorSpanRef = changeColorBtnRef.previousElementSibling.firstElementChild;
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+changeColorBtnRef.addEventListener('click', onChangeColorBtnRefClick);
+function onChangeColorBtnRefClick() {
+	document.body.style.backgroundColor = getRandomHexColor();
+	colorSpanRef.textContent = getRandomHexColor();
 }
