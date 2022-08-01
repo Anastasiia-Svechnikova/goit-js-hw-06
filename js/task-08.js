dd1bc3d0e435d9e319703e52b2a02306adaf1f72
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-08-01 23:36:38 +0300

    task8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+	event.preventDefault();
+
+	const { email, password } = event.currentTarget.elements;
+
+	if (email.value === '' || password.value === '') {
+		alert('Все поля должны быть заполнены');
+	} else {
+		const elements = {
+			email: email.value,
+			password: password.value,
+		};
+
+		console.log(`Email: ${elements.email}\nPassword: ${elements.password}`);
+		event.currentTarget.reset();
+	}
+}
